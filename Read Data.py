c59d7309f1f7e165acfabb73707c79e598d79436
KyleNGH <KyleNGH@gmail.com>
2018-11-28 22:46:02 -1000

    updated weather pull

--- Read Data.py
+++ Read Data.py
@@ -5,10 +5,27 @@ File: Read Data from file
 
 '''
 
+### pull current weather 
+def get_weather():
+    import pyowm
+
+    # api key
+    owm = pyowm.OWM("b508f902bfd4f9d38491e1b8557c675f")
+
+    observation = owm.weather_at_place("Honolulu,us")
+
+    weather = observation.get_weather()
+
+    status = weather.get_status()
+    
+    return status
 
 
 
+### main ###
 
+status = get_weather()
+print(status)
 
 
 
